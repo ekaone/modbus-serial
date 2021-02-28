a0f577631cbddfba630077f7e94c5fcb6766c5ca
ekaone <ekaone3033@gmail.com>
2021-02-28 10:09:21 +0700

    test

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log(newFunction());
+// create an empty modbus client
+var SerialPort = require("serialport");
+var ModbusRTU = require("modbus-serial");
+// var client = new ModbusRTU();
+var serialPort = new SerialPort("COM5", {
+  baudRate: 38400,
+  // autoOpen: true,
+  parity: "none",
+  dataBits: 8,
+});
+
+// open connection to a serial port
+// client.connectRTUBuffered("COM5", { baudRate: 38400, parity: "none" });
+var client = new ModbusRTU(serialPort);
+client.setID(2);
+
+// read the values of 10 registers starting at address 0
+// on device number 1. and log the values to the console.
+setInterval(function () {
+  client.readHoldingRegisters(31002, 2, function (err, data) {
+    console.log(data.data);
+
+    // try {
+    //   console.log(data.data);
+    // } catch (err) {
+    //   console.error(err);
+    // }
+  });
+}, 1000);
--- js/loggerTCP.js
+++ js/loggerTCP.js
@@ -3,7 +3,7 @@ var ModbusRTU = require("modbus-serial");
 var client = new ModbusRTU();
 
 // open connection to a tcp line
-client.connectTCP("127.0.0.1", { port: 8502 });
+client.connectTCP("127.0.0.1", { port: 502 });
 client.setID(1);
 
 // read the values of 10 registers starting at address 0
